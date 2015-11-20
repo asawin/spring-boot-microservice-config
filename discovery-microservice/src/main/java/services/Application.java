d30a332ab51722c3351cc493a649cdc46433a8f8
golf <asawin@gmail.com>
2015-11-20 16:01:41 +0700

    init

---
+++ discovery-microservice/src/main/java/services/Application.java
@@ -0,0 +1,16 @@
+package services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by golf on 11/20/15.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
